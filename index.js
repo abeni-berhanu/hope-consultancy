5044650b67c43763894a92f87c783ac6300f3083
abeni126 <abenezer.berhanu.gs@gmail.com>
2022-12-09 03:08:48 +0100

    msg

--- index.js
+++ index.js
@@ -4,3 +4,26 @@ const options = document.getElementsByClassName("header-right")[0];
 menu.addEventListener("click", () => {
     options.classList.toggle("active");
 })
+
+
+
+const header = document.querySelector(".header");
+const welcomeHeader = document.querySelector(".welcome-header");
+const mainDiv = document.querySelector(".main-div");
+const intersectionOptions = {
+    rootMargin: "-70px 0px 0px 0px"
+};
+
+const welcomeHeaderObserver = new IntersectionObserver(function(entries, intersectionOptions) {
+    entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                header.classList.add("intersected");
+            }
+            else {
+                header.classList.remove("intersected")
+            }
+
+        })
+    },intersectionOptions);
+
+welcomeHeaderObserver.observe(welcomeHeader);
