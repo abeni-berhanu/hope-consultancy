19340594c0bd40bb482b804222aa88b98b7a7a04
abeni126 <abenezer.berhanu.gs@gmail.com>
2022-12-12 16:18:40 +0100

    msg

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const menu = document.getElementsByClassName("menu")[0];
 const options = document.getElementsByClassName("header-right")[0];
+const destinations = document.querySelectorAll(".destination");
+const header = document.querySelector(".header");
+const welcomeHeader = document.querySelector(".welcome-header");
+const mainDiv = document.querySelector(".main-div");
 
+//nav-bar resposivness
 menu.addEventListener("click", () => {
     options.classList.toggle("active");
 })
 
-
-
-const header = document.querySelector(".header");
-const welcomeHeader = document.querySelector(".welcome-header");
-const mainDiv = document.querySelector(".main-div");
+//header scroll down change
 const intersectionOptions = {
     rootMargin: "-70px 0px 0px 0px"
 };
-
 const welcomeHeaderObserver = new IntersectionObserver(function(entries, intersectionOptions) {
     entries.forEach(entry => {
             if (!entry.isIntersecting) {
@@ -27,3 +27,22 @@ const welcomeHeaderObserver = new IntersectionObserver(function(entries, interse
     },intersectionOptions);
 
 welcomeHeaderObserver.observe(welcomeHeader);
+
+/* //destinations fade-in to scroll
+const destinationIntersectionOptions = {
+}
+const destinationFadeInObserver = new IntersectionObserver(function(entries, destinationIntersectionOptions){
+    entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return
+            }
+            else {
+                entry.target.classList.add("fade-in");
+                destinationFadeInObserver.unobserve(entry.target);
+                }
+            })
+        }, destinationIntersectionOptions);
+
+destinations.forEach(fadeIn => {
+    destinationFadeInObserver.observe(fadeIn);
+}) */
